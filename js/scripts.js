5f65c636ccd2e1e375cc4db50db6b36d0423939a
Kate Trahan and Natalia Telpukhova <natalia@>
2017-12-06 08:28:18 -0800

    add initial spec, check functionality of singular, simple behavior

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+//Back End Logic
+
+var leapYear = function(year) {
+  return false;
+};
+
+
+
+
+
+// Front End Logic
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
