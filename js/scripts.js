f1a13c77ef699448e7b91a989c2fdb986f328c87
Kate Trahan and Natalia Telpukhova <natalia@>
2017-12-06 08:54:18 -0800

    add spec; simple conditional

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,11 @@
 //Back End Logic
 
 var leapYear = function(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0)) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
   } else {
     return false;
   }
-
 };
 
 
